5d1806a8ada6381eab944efb0dd18ce8c09f05f5
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-14 15:36:26 +0200

    updated cities list view

--- cities_app/views.py
+++ cities_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
 from .models import City
 from .serializers import CitySerializer
-
 # Create your views here.
 
 
-class CityList(RetrieveAPIView):
-    queryset = City.objets.all()
+# Retrieve is reserved for 'by ID'!
+class CityList(ListCreateAPIView):
+    queryset = City.objects.all()
     serializer_class = CitySerializer
