146f16569e72be2eaf812d57f9bae23b8c76b719
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-14 11:30:44 +0200

    added city model and seededcity data

--- cities_app/models.py
+++ cities_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class City(models.Model):
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50, blank=True)
+    continent = models.CharField(max_length=50)
+    description = models.TextField()
+    top_3_attractions = ArrayField(models.CharField(max_length=50))
+    # image = models.ImageField(max_length=250)
+    image = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f'{self.city}, {self.country}'
