523833e7b9a3f0cfa12f2e9806cf215fed59ce53
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-14 14:13:20 +0200

    added city retrieve view and serialiser

--- cities_app/views.py
+++ cities_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from .models import City
+from .serializers import CitySerializer
 
 # Create your views here.
+
+
+class CityList(RetrieveAPIView):
+    queryset = City.objets.all()
+    serializer_class = CitySerializer
