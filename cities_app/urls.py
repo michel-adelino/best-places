d633b3a9be9056baec1ce9854e0d6261211d49bd
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-16 15:06:37 +0200

    added a 'search cities' view

--- cities_app/urls.py
+++ cities_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CityListCreate, CityRetrieveUpdateDelete, ScrapeSearch
+from .views import CityListCreate, CityRetrieveUpdateDelete, ScrapeSearch, SearchCity
 
 urlpatterns = [
     path('cities/', CityListCreate.as_view()),
     path('cities/<int:pk>/', CityRetrieveUpdateDelete.as_view()),
     path('scrape/search/', ScrapeSearch.as_view()),
+    path('cities/<str:search_term>/', SearchCity.as_view()),
 ]
