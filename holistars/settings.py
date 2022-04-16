f8d492222b82d99da3fbb68d40c91496e61770c5
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-16 23:05:43 +0200

    fixed search for accented characters

--- holistars/settings.py
+++ holistars/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',  # supports search on accented chars
     'corsheaders',
     'rest_framework',
     'jwt_auth_app',
