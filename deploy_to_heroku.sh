d2174ec369826f33cb3dafe579d5506d1c52f213
michel-adelino <michel.adelino.tech@gmail.com>
2022-04-25 18:03:11 +0200

    refreshed seed data and updated deployment script

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Update heroku's git repo (the BE deployed repo)
 git push heroku main
 heroku run python manage.py makemigrations
 heroku run python manage.py migrate
+
+# Re-seed data
+heroku run python manage.py loaddata seed_users.json
+heroku run python manage.py loaddata seed_cities.json
+heroku run python manage.py loaddata seed_holidays.json
+heroku run python manage.py loaddata seed_reviews.json
+heroku run python manage.py loaddata seed_followers.json
